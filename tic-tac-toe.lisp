8936aa04b51b8cfff588e9a0c78c455533a85f1b
kar7hik <karthikkumar.s@protonmail.com>
2018-09-04 18:39:07 +0530

    tic-tac-toe v0.2

--- tic-tac-toe.lisp
+++ tic-tac-toe.lisp
@@ -1,27 +1,27 @@
 
-;;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; -*-
-;;;
-;;; Tic-Toc-Toe Game: Plays and explains the strategy behind each move. 
-;;;
-;;; Copyright © 2018 Karthik Kumar <karthikkumar.s@protonmail.com>
-;;;
-;;; This program is free software: you can redistribute it and/or
-;;; modify it under the terms of the GNU General Public License as
-;;; published by the Free Software Foundation, either version 3 of the
-;;; License, or (at your option) any later version.
-;;;
-;;; This program is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-;;; General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License along
-;;; with this program. If not, see <http://www.gnu.org/licenses/>.
+ ;;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; -*-
+ ;;;
+ ;;; Tic-Toc-Toe Game: Plays and explains the strategy behind each move. 
+ ;;;
+ ;;; Copyright © 2018 Karthik Kumar <karthikkumar.s@protonmail.com>
+ ;;;
+ ;;; This program is free software: you can redistribute it and/or
+ ;;; modify it under the terms of the GNU General Public License as
+ ;;; published by the Free Software Foundation, either version 3 of the
+ ;;; License, or (at your option) any later version.
+ ;;;
+ ;;; This program is distributed in the hope that it will be useful,
+ ;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+ ;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ ;;; General Public License for more details.
+ ;;;
+ ;;; You should have received a copy of the GNU General Public License along
+ ;;; with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
-;;;;;;;;;;;;;;;;;;;;;;;
+ ;;;;;;;;;;;;;;;;;;;;;;;
 ;; Tic-Toc-Toe Game: ;;
-;;;;;;;;;;;;;;;;;;;;;;;
+ ;;;;;;;;;;;;;;;;;;;;;;;
 
 (defun make-board ()
   "Creates a board. Consist of the symbol BOARD followed by 
