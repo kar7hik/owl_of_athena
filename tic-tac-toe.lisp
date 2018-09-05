dac5bc268142e6af7f714d63f04a5798d6723b38
kar7hik <karthikkumar.s@protonmail.com>
2018-09-05 11:57:20 +0530

    tic-tac-toe v0.3 - refined

--- tic-tac-toe.lisp
+++ tic-tac-toe.lisp
@@ -1,45 +1,43 @@
 
- ;;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; -*-
- ;;;
- ;;; Tic-Toc-Toe Game: Plays and explains the strategy behind each move. 
- ;;;
- ;;; Copyright © 2018 Karthik Kumar <karthikkumar.s@protonmail.com>
- ;;;
- ;;; This program is free software: you can redistribute it and/or
- ;;; modify it under the terms of the GNU General Public License as
- ;;; published by the Free Software Foundation, either version 3 of the
- ;;; License, or (at your option) any later version.
- ;;;
- ;;; This program is distributed in the hope that it will be useful,
- ;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
- ;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- ;;; General Public License for more details.
- ;;;
- ;;; You should have received a copy of the GNU General Public License along
- ;;; with this program. If not, see <http://www.gnu.org/licenses/>.
+;;; -*- Mode: Lisp; Syntax: ANSI-Common-Lisp; -*-
+;;;
+;;; Tic-Toc-Toe Game: Plays and explains the strategy behind each move. 
+;;;
+;;; Copyright © 2018 Karthik Kumar <karthikkumar.s@protonmail.com>
+;;;
+;;; This program is free software: you can redistribute it and/or
+;;; modify it under the terms of the GNU General Public License as
+;;; published by the Free Software Foundation, either version 3 of the
+;;; License, or (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License along
+;;; with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
- ;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;
 ;; Tic-Toc-Toe Game: ;;
- ;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;
 
-(defun make-board ()
-  "Creates a board. Consist of the symbol BOARD followed by 
-nine numbers. zero - empty; one - O; ten - X"
-  (list 'board 0 0 0 0 0 0 0 0 0))
 
 
 (defparameter *computer* 10)
 (defparameter *user* 1)
-;; (make-move *user* 5 b)
-;; (make-move *computer* 3 b)
-;; (print-board b)
-
 (defparameter *triplets*
   '((1 2 3) (4 5 6) (7 8 9) ;Horizontal triplets.
 	(1 4 7) (2 5 8) (3 6 9) ;Vertical triplets.
 	(1 5 9) (3 5 7))) ;Diagonal triplets.
 
+
+
+(defun make-board ()
+  "Creates a board. Consist of the symbol BOARD followed by 
+nine numbers. zero - empty; one - O; ten - X"
+  (list 'board 0 0 0 0 0 0 0 0 0))
 
 
 (defun convert-to-letter (v)
@@ -55,6 +53,7 @@ nine numbers. zero - empty; one - O; ten - X"
 		  (convert-to-letter x)
 		  (convert-to-letter y)
 		  (convert-to-letter z)))
+
 
 (defun print-board (board)
   "Displays the entire board"
@@ -73,14 +72,17 @@ nine numbers. zero - empty; one - O; ten - X"
   (format t "~%~%"))
 
 
-(defparameter b (make-board))
-(print-board b)
-
 (defun make-move (player pos board)
   "destructively changing one of the board positions from zero to 1 (for O)
 or a 10 (for X)"
   (setf (nth pos board) player)
   board)
+
+
+;; (make-move *user* 5 b)
+;; (make-move *computer* 3 b)
+;; (print-board b)
+
 
 
 (defun sum-triplets (board triplets)
@@ -105,12 +107,6 @@ if sum of any triplets is either 3 or 30, then there is a winner."
 		(member (* 3 *user*) sums))))
 
 
-(defun play-one-game ()
-  "Offers the user the choice to go first, and then call either computer-move
-or opponent-move as appropriate"
-  (if (y-or-n-p "Would you like to go first? ")
-	  (user-move (make-board))
-	  (computer-move (make-board))))
 
 (defun read-a-legal-move (board)
   "Checks whether the entered move is legal."
@@ -130,6 +126,7 @@ or opponent-move as appropriate"
 (defun board-full-p (board)
   "Tests if there are no more empty spaces left on the board"
   (not (member 0 board)))
+
 
 (defun user-move (board)
   "Give chance to user."
@@ -162,18 +159,6 @@ or opponent-move as appropriate"
 		  (t (user-move new-board)))))
 
 
-;; (defun choose-best-move (board)
-;;   "Choosing best move."
-;;   (random-move-strategy board))
-
-
-(defun random-move-strategy (board)
-  "Pick random"
-  (list (pick-random-empty-position board)
-		"random move"))
-
-
-
 (defun pick-random-empty-position (board)
   "Picks a random number from one to nine."
   (let ((pos (+ 1 (random 9))))
@@ -182,17 +167,16 @@ or opponent-move as appropriate"
 		(pick-random-empty-position board))))
 
 
-(defun make-three-in-a-row (board)
-  "Intelligence"
-  (let ((pos (win-or-block board
-						   (* 2 *computer*))))
-	(and pos (list pos "make three in a row"))))
+(defun random-move-strategy (board)
+  "Pick random"
+  (list (pick-random-empty-position board)
+		"random move"))
 
-(defun block-opponent-win (board)
-  "Blocking"
-  (let ((pos (win-or-block board
-						   (* 2 *computer*))))
-	(and pos (list pos "block opponent"))))
+
+(defun find-empty-position (board squares)
+  (find-if #'(lambda (pos)
+			   (zerop (nth pos board)))
+		   squares))
 
 
 (defun win-or-block (board target-sum)
@@ -205,16 +189,35 @@ or opponent-move as appropriate"
 	(when triplet
 	  (find-empty-position board triplet))))
 
-(defun find-empty-position (board squares)
-  (find-if #'(lambda (pos)
-			   (zerop (nth pos board)))
-		   squares))
+
+(defun make-three-in-a-row (board)
+  "Completing the row for winning"
+  (let ((pos (win-or-block board
+						   (* 2 *computer*))))
+	(and pos (list pos "make three in a row"))))
+
+
+(defun block-opponent-win (board)
+  "Blocking the opponent if they have more than two in a row"
+  (let ((pos (win-or-block board
+						   (* 2 *user*))))
+	(and pos (list pos "block opponent"))))
+
 
 (defun choose-best-move (board)
-  "Choosing best move."
+  "Choosing best move. Kind of intelligent."
   (or (make-three-in-a-row board)
 	  (block-opponent-win board)
 	  (random-move-strategy board)))
+
+
+(defun play-one-game ()
+  "Offers the user the choice to go first, and then call either computer-move
+or opponent-move as appropriate"
+  (if (y-or-n-p "Would you like to go first? ")
+	  (user-move (make-board))
+	  (computer-move (make-board))))
+
 
 (play-one-game)
 
